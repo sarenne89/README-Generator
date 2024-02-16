3e7003f11ad406f80a0d8505b76913827b9526e3
Matt Dunmore <mdunmore89@gmail.com>
2024-02-16 18:50:23 +0000

    changed inquirer install to older version

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 const path = require('path');
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    
 ];
 
 // function to write README file
